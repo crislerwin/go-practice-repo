044d37b5922155b695159c4facc38cc9e1b24fd3
crislerwin <crislerwintler@gmail.com>
2023-12-13 08:58:14 -0300

    feat: dynamic array check is empty, add and remove

---
+++ data-structures/dynamicarray/dynamicarray.go
@@ -0,0 +1,59 @@
+package dynamicarray
+
+import "errors"
+
+type DynamicArray struct {
+	Size        int
+	Capacity    int
+	ElementData []any
+}
+
+var defaultCapacity = 10
+
+func (da *DynamicArray) IsEmpty() bool {
+	return da.Size == 0
+}
+
+func (da *DynamicArray) Add(element any) {
+	if da.Size == da.Capacity {
+		da.NewCapacity()
+	}
+	da.ElementData[da.Size] = element
+	da.Size++
+}
+
+func (da *DynamicArray) NewCapacity() {
+	if da.Capacity == 0 {
+		da.Capacity = defaultCapacity
+	} else {
+		da.Capacity = da.Capacity << 1
+	}
+
+	newDataElement := make([]any, da.Capacity)
+
+	copy(newDataElement, da.ElementData)
+
+	da.ElementData = newDataElement
+}
+
+func (da *DynamicArray) GetData() []any {
+	return da.ElementData[:da.Size]
+}
+
+func (da *DynamicArray) Remove(index int) error {
+	err := da.CheckRangeFromIndex(index)
+	if err != nil {
+		return err
+	}
+	copy(da.ElementData[index:], da.ElementData[index+1:da.Size])
+	da.ElementData[da.Size-1] = nil
+	da.Size--
+	return nil
+}
+
+func (da *DynamicArray) CheckRangeFromIndex(index int) error {
+	if index >= da.Size || index < 0 {
+		return errors.New("index out of range")
+	}
+	return nil
+}
---
+++ data-structures/dynamicarray/dynamicarray_test.go
@@ -0,0 +1,45 @@
+package dynamicarray
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDynamicArray(t *testing.T) {
+	numbers := DynamicArray{}
+
+	t.Run("Check empty Dynamic Array", func(t *testing.T) {
+		if numbers.IsEmpty() != true {
+			t.Errorf("Expected true but got %v", numbers.IsEmpty())
+		}
+	})
+
+	numbers.Add(10)
+	numbers.Add(20)
+	numbers.Add(30)
+	numbers.Add(40)
+	numbers.Add(50)
+
+	t.Run("Add Element into Dynamic Array", func(t *testing.T) {
+		if numbers.IsEmpty() != false {
+			t.Errorf("Expected be false but got %v", numbers.IsEmpty())
+		}
+		var res []any = []any{10, 20, 30, 40, 50}
+		if !reflect.DeepEqual(numbers.GetData(), res) {
+			t.Errorf("Expected be [10, 20, 30, 40, 50] but got %v", numbers.GetData())
+		}
+	})
+	t.Run("Remove in Dynamic Array", func(t *testing.T) {
+		if numbers.IsEmpty() != false {
+			res := []any{10, 20, 40, 50}
+
+			err := numbers.Remove(1)
+			if err != nil {
+				t.Errorf("Expected be [10, 30, 40, 50] but got an Error %v", err)
+			}
+			if !reflect.DeepEqual(numbers.GetData(), res) {
+				t.Errorf("Expected be [10, 30, 40, 50], but got %v", numbers.GetData())
+			}
+		}
+	})
+}
